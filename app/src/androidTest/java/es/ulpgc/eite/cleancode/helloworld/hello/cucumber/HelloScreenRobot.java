ff83805384c08564198db72544d4bd6b9dd3cb13
Luis Hernandez <lhdez@iuma.ulpgc.es>
2020-03-24 12:48:25 +0000

    incluyendo test para TDD usando Cucumber para BDD

---
+++ app/src/androidTest/java/es/ulpgc/eite/cleancode/helloworld/hello/cucumber/HelloScreenRobot.java
@@ -0,0 +1,55 @@
+package es.ulpgc.eite.cleancode.helloworld.hello.cucumber;
+
+import androidx.test.rule.ActivityTestRule;
+
+import es.ulpgc.eite.cleancode.helloworld.R;
+import es.ulpgc.eite.cleancode.helloworld.hello.HelloActivity;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+public class HelloScreenRobot {
+
+    private ActivityTestRule<HelloActivity> testRule = new ActivityTestRule<>(
+        HelloActivity.class, false, false
+    );
+
+    public void encontrandonosEnPantallaHello() {
+        testRule.launchActivity(null);
+    }
+
+    public void mostrandoBotonSayHelloActivado() {
+        onView(withId(R.id.sayHelloButton))
+            .check(matches(isDisplayed()))
+            .check(matches(isEnabled()));
+    }
+
+    public void mostrandoBotonGoByeActivado() {
+        onView(withId(R.id.goByeButton))
+            .check(matches(isDisplayed()))
+            .check(matches(isEnabled()));
+    }
+
+    public void pulsemosBotonSayHello() {
+        onView(withId(R.id.sayHelloButton)).perform(click());
+    }
+
+
+    public void mostrandoMensajeVacio() {
+        onView(withId(R.id.helloMessage))
+            .check(matches(isDisplayed()))
+            .check(matches(withText("?")));
+    }
+
+    public void mostrandoMensajeHelloWorld() {
+        onView(withId(R.id.helloMessage))
+            .check(matches(isDisplayed()))
+            .check(matches(withText("Hello World!")));
+    }
+
+}
---
+++ app/src/androidTest/java/es/ulpgc/eite/cleancode/helloworld/hello/cucumber/HelloScreenTest.java
@@ -0,0 +1,38 @@
+package es.ulpgc.eite.cleancode.helloworld.hello.cucumber;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class HelloScreenTest {
+
+
+    public HelloScreenRobot robot = new HelloScreenRobot();
+
+    @Test
+    public void testDecirHelloWorldEnPantallaHello() {
+
+        // Given encontrándonos en pantalla "Hello"
+        robot.encontrandonosEnPantallaHello();
+        // And mostrando mensaje "?"
+        robot.mostrandoMensajeVacio();
+        // And mostrando botón "Say Hello" activado
+        robot.mostrandoBotonSayHelloActivado();
+        // And mostrando botón "Go Bye" activado
+        robot.mostrandoBotonGoByeActivado();
+
+        // When pulsemos botón "Say Hello"
+        robot.pulsemosBotonSayHello();
+
+        // Then mostrando botón "Say Hello" activado
+        robot.mostrandoBotonSayHelloActivado();
+        // And mostrando botón "Go Bye" activado
+        robot.mostrandoBotonGoByeActivado();
+        // And mostrando mensaje "Hello World!"
+        robot.mostrandoMensajeHelloWorld();
+    }
+}
